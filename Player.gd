3732d9fd2be3fb914e47a527bf5136db3b41878a
Yevgeni "Jay" Rise <rise_yevgeni@kurusugawa.jp>
2024-01-27 17:32:28 +0900

    Add spinning when laughing

--- Player.gd
+++ Player.gd
@@ -11,10 +11,13 @@ extends Node3D
 @export var addPitchScaleSpeed : float = 0.1
 
 var initialVolume : float
-var pushTime : float =0.0
+var pushTime : float = 0.0
+var fliptimer : float = 0.0
+var fliptimerlimit : float = 0.45
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	global_rotation = Vector3(0,0,0)
 	initialVolume = $AudioStreamPlayer.volume_db
 	pass # Replace with function body.
 
@@ -26,12 +29,20 @@ func _process(delta):
 	if(Input.is_action_just_pressed("Laugh")):   
 		$AudioStreamPlayer.play()
 	if (Input.is_action_pressed("Laugh")):
+			global_rotation = Vector3(0,0,90)
+			if(fliptimer<0):
+				$GobotSkin.flip()
+				fliptimer = fliptimerlimit
+			else:
+				fliptimer -= delta
 			$AudioStreamPlayer.volume_db +=2*delta
 			$AudioStreamPlayer.pitch_scale+=0.1*delta
 			get_parent()._addLaughRadius(laughSpreadSpeed*delta)
 			get_parent()._addLaughGagePoint((pushTime*0.5+laughPointSpeed)*delta)
 			pushTime+=delta;
 	else:
+		global_rotation = Vector3(0,0,0)
+		$GobotSkin.idle()
 		$AudioStreamPlayer.stop()
 		$AudioStreamPlayer.volume_db = initialVolume
 		$AudioStreamPlayer.pitch_scale=1
