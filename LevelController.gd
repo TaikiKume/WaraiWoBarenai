a018504afec6f7a60c0cb9300a51f6282b781bcc
Yevgeni "Jay" Rise <rise_yevgeni@kurusugawa.jp>
2024-01-27 22:29:14 +0900

    Added door explosion and disco

--- LevelController.gd
+++ LevelController.gd
@@ -13,9 +13,16 @@ var progressTimer : float = 0.0
 #クリア時間
 @export var gameOverTime : float = 60.0
 
-var isStartGame : bool =false
+var isStartGame : bool = false
 
 var count : int =0
+
+@export var Door : MeshInstance3D
+@export var DoorLight : OmniLight3D
+@export var DoorLight2 : OmniLight3D
+@export var RoomLight : OmniLight3D
+
+var DoorBroken : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,8 +34,6 @@ func _process(delta):
 	if(!isStartGame):
 		return 0
 	
-	
-	
 	progressbar.value = laughGagePoint
 	print(laughGagePoint)
 	
@@ -36,21 +41,45 @@ func _process(delta):
 #Clear!!!!
 	if(laughGagePoint>=100):
 		get_tree().change_scene_to_file("res://game_clear.tscn")
-		print("GameClear");
 		return 1
 
 #TimeOver判定
 	progressTimer += delta;
 	if(progressTimer>gameOverTime):
 		get_tree().change_scene_to_file("res://game_over.tscn")
-		print("TimeOver");
 
 
 #プレイヤーの死亡判定をする
 	if($Oni.global_transform.origin.length()<$Oni.room_burst_distance&&laughRadius>0.1):
-		
 		get_tree().change_scene_to_file("res://game_over.tscn")
-		print("Die");	
+		
+	if laughRadius>0.1:
+		RoomLight.light_energy += 1*delta
+		RoomLight.light_color.h += delta
+		RoomLight.light_color.s += delta
+	else:
+		RoomLight.light_energy = 1
+		RoomLight.light_color.h = 0
+		RoomLight.light_color.s = 0
+		
+		
+	if($Oni.global_transform.origin.length()<$Oni.room_burst_distance*1.2&&not DoorBroken):
+		DoorBroken = true
+		Door.visible = false
+		Door.get_child(0).playing=true
+		DoorLight.light_energy =16
+		DoorLight2.light_energy =16
+		
+	if($Oni.global_transform.origin.length()>$Oni.room_burst_distance*1.2&&DoorBroken):
+		DoorBroken = false
+		Door.visible = true
+	
+	if DoorLight.light_energy > 0:
+		DoorLight.light_energy -= 10*delta
+		DoorLight2.light_energy -= 10*delta
+	else:
+		DoorLight.light_energy = 0
+		DoorLight2.light_energy = 0
 	
 
 
