86f24e2350fe58d0cc2e3f16b49de301b838352e
Yevgeni "Jay" Rise <rise_yevgeni@kurusugawa.jp>
2024-01-28 14:48:47 +0900

    Oni2 early

--- global.gd
+++ global.gd
@@ -1,10 +1,17 @@
 extends Node
 
 
-var difficulty : int = 0;
+var difficulty : int = 0
+var level : int = 0
 
 var difficulty_onis_level1: Array = [
 	[10, 1.0, 0.5,Vector3(-1,0,0), 150,5,10,300],
 	[10, 1.5, 0.5,Vector3(-1,0,0), 120,5,10,270],
 	[10, 2.0, 0.5,Vector3(-1,0,0), 100,5,10,240],
+]
+
+var difficulty_onis_level2: Array = [
+	[15, 1.0, 0.3,Vector3(1,0,0), 100,5,20,10],
+	[15, 1.5, 0.3,Vector3(1,0,0), 70,5,20,10],
+	[15, 2.0, 0.3,Vector3(1,0,0), 50,5,20,10],
 ]
